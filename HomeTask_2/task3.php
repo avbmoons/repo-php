0897687ccdfff907a7110704739ed46e59462659
avbmoons <88578581+avbmoons@users.noreply.github.com>
2022-05-04 12:14:33 +0300

    for task3

--- HomeTask_2/task3.php
+++ HomeTask_2/task3.php
@@ -36,38 +36,3 @@ if (!($number > 0)) {
             break;
     }
 }
-
-// if (!($number > 0)) {
-//     goto start;
-// } else {
-
-//     switch ($number) {
-//         case 1:
-//             echo "большой";
-//             break;
-//         case 2:
-//             echo "указательный";
-//             break;
-//         case 3:
-//             echo "средний";
-//             break;
-//         case 4:
-//             echo "безымянный";
-//             break;
-//         case 5:
-//             echo "мизинец";
-//             break;
-//         case 6:
-//             echo "безымянный";
-//             break;
-//         case 7:
-//             echo "средний";
-//             break;
-//         case 8:
-//             echo "указательный";
-//             break;
-//         case 9:
-//             echo "большой";
-//             break;
-//     }
-// }
